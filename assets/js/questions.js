618c0b47c565e1f3c081f9a45e38811eddc093ac
keeezy <lazarokev@gmail.com>
2022-04-14 01:25:58 -0700

    declared a global variable for questions, created an array for the choices

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "Who is SpongeBob's best friend?",
+        choices: ["Gary", "Squidward", "Patrick", "Plankton"],
+        answer: "Patrick"
+    },
+    {
+        title: "Patrick is what type of animal?",
+        choices: ["Pufferfish", "Starfish", "Jellyfish", "Clownfish"],
+        answer: "Starfish"
+    },
+    {
+        title: "Where does SpongeBob live, according to the opening theme?",
+        choices: ["A pineapple under the sea", "A fish tank in a pet shop", "An apartment on 21st street", "A Mr. Clean Magic Eraser Sponge Box"],
+        answer: "A pineapple under the sea"
+    },
+    {
+        title: "What is Spongebob's personal motto?",
+        choices: ["Everyone's an idiot except me", "I'm ready!", "Tommorow will be better", "Another day, another dollar"],
+        answer: "I'm ready!"
+    },
+    {
+        title: "Who is Plankton's wife?",
+        choices: ["Donna the Muntant Clam", "Nikki the Plankton", "Karen the Mark II Univac W.I.F.E. Computer", "Shellie the Evil Worm"],
+        answer: "Karen the Mark II Univac W.I.F.E. Computer"
+    },
+    {
+        title: "Who is the voice actor for SpongeBob?",
+        choices: ["Bill Fagerbakke", "Clancy Brown", "Roger Bumpass", "Tom Kenny"],
+        answer: "Tom Kenny"
+    }
+]
