60b03b489c7813574ad0598a92e5a88b6606e044
keeezy <lazarokev@gmail.com>
2022-04-15 23:27:53 -0700

    bugs

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -13,6 +13,9 @@ var questionsEl = document.querySelector(".hide")
 var startMenuEl = document.getElementById("start-menu")
 var initalsEl = document.getElementById("initials")
 
+
+
+// Function to start trivia
 function startQuiz() {
     //start menu is hidden
     startMenuEl.setAttribute("class", "hide");
@@ -23,6 +26,64 @@ function startQuiz() {
     // start timeclock
     timer = setInterval(clockCount,1000)
     timer.textContent = timeLeft;
+    
+    showQuestion();
+}
+
+// Function to show questions
+function showQuestion() {
+    
+    
+    // Current question in array
+    var currentQ = questions[currentQuestionIndex];
+    
+    // Title with current question is shown
+    var titleEl = document.getElementById("question-title");
+    
+    // created variable title element, pulling title from questions array object
+    // text content changes to current question title using dot notation
+    titleEl.textContent = currentQ.title;
+    
+    //clears old questions
+    choicesEl.innerHTML = "",
+
+    //loop choices
+    currentQ.choices.forEach(function(choice) {
+        // create a button
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+
+        choiceNode.textContent = choice;
+
+        choiceNode.onclick = questionClick;
+
+        choicesEl.appendChild(choiceNode);
+    });
+    
+
+}
+
+function questionClick() {
+    if (this.value === questions[currentQuestionIndex].answer) {
+        alert ("correct!!!!");
+    } else {
+        alert ("incorrect!!!!")
+        timeLeft -=10;
+        
+        
+        }
+    
+
+    
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex === questions.length) {
+        quizEnd();
+    } else {
+        showQuestion();
+    }
+
 
 }
 
@@ -39,3 +100,26 @@ function clockCount() {
 }
 
 startBtn.onclick = startQuiz;
+console.log(showQuestion)
+
+
+
+// function questionClick() {
+//     if (this.value === questions[currentQuestionIndex].answer) {
+//         timeLeft -=10;
+//         console.log("incorrect answer!")
+
+//         if (timeLeft <0) {
+//             timeLeft= 0;
+//         }
+
+//         timeEl.textContent = timeLeft;
+
+//         alert("Wrong answer!");
+
+
+//     } else {
+//         alert("Correct!!!");
+
+//     }
+// }
